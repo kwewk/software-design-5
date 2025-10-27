4daec984c7a16d6025ebd0442d06e135d2a33965
kwewk <egor.a.gavrilin@gmail.com>
2025-10-27 13:31:37 +0100

    chore: routes configured

---
+++ src/routes/v1/meals.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+import { list, show, create, edit, destroy } from 'controllers/meals';
+import { checkJwt } from 'middleware/checkJwt';
+
+const router = Router();
+
+router.get('/', [checkJwt], list);
+router.get('/:id([0-9]+)', [checkJwt], show);
+router.post('/', [checkJwt], create);
+router.patch('/:id([0-9]+)', [checkJwt], edit);
+router.delete('/:id([0-9]+)', [checkJwt], destroy);
+
+export default router;
