967daf1eade8f301dd3c8ebb977e6aaad4f93e3f
kwewk <egor.a.gavrilin@gmail.com>
2025-10-27 13:33:28 +0100

    chore: routers added

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
+import appUsers from './appUsers';
 import auth from './auth';
+import meals from './meals';
+import recipes from './recipes';
 import users from './users';
 
 const router = Router();
 
 router.use('/auth', auth);
 router.use('/users', users);
+router.use('/recipes', recipes);
+router.use('/meals', meals);
+router.use('/app-users', appUsers);
 
 export default router;
